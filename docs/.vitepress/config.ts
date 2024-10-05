0d92429a196f76e0b5ea22e215f58234011403ef
eklairs <eklairs@proton.me>
2024-10-05 15:09:09 +0530

    docs: initialize docs with vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vitepress"
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Dooit Bar Utils",
+  description: "Bar utils for Dooit",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+    ],
+    socialLinks: [
+      { icon: "github", link: "https://github.com/dooit-org/dooit-bar-utils" },
+      { icon: "discord", link: "https://discord.com/invite/WA2ER9MBWa" },
+      { icon: "twitter", link: "https://twitter.com/kraanzu" },
+    ],
+    search: {
+      provider: "local"
+    }
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+---
+layout: home
+
+hero:
+  name: "Dooit Bar Utils"
+  tagline: "Bar utils for Dooit"
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
