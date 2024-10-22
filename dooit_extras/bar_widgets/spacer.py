0bdc77959c8636d4404e85d4a5835ebe4668ca78
kraanzu <kraanzu@gmail.com>
2024-10-22 19:01:58 +0530

    accept any params

--- dooit_extras/bar_widgets/spacer.py
+++ dooit_extras/bar_widgets/spacer.py
@@ -7,7 +7,7 @@ from ._base import BarUtilWidgetBase
 
 class Spacer(BarUtilWidgetBase):
     def __init__(self, api: DooitAPI, width: int, fg: str = "", bg: str = "") -> None:
-        super().__init__(func=lambda: " ", width=width, api=api)
+        super().__init__(func=lambda *_: " ", width=width, api=api)
 
         self.fg = fg
         self.bg = bg
