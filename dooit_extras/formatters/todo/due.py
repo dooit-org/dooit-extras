328e07b52d08368ec400a5454ff5b4919de373e2
kraanzu <kraanzu@gmail.com>
2024-10-22 09:55:10 +0530

    formatter: todo danger function

--- dooit_extras/formatters/todo/due.py
+++ dooit_extras/formatters/todo/due.py
@@ -1,5 +1,8 @@
 from typing import Optional
+from rich.style import Style
+from rich.text import Text
 from dooit.api.todo import datetime, Todo
+from dooit.ui.api import DooitAPI
 
 
 def causal_date_format(due: Optional[datetime], _: Todo) -> str:
@@ -16,3 +19,17 @@ def causal_date_format(due: Optional[datetime], _: Todo) -> str:
         dt_format += " (%-I:%M)"
 
     return due.strftime(dt_format)
+
+
+def danger_today(due: Optional[datetime], _: Todo, api: DooitAPI) -> Optional[str]:
+    if not due:
+        return ""
+
+    if due.date() == datetime.today().date():
+        return Text(
+            "Today",
+            style=Style(
+                color=api.vars.theme.red,
+                bold=True,
+            ),
+        ).markup
