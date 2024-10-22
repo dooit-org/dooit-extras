6dc9b1b6559fd025fb1f01f263ae72967a9082cd
kraanzu <kraanzu@gmail.com>
2024-10-22 21:51:59 +0530

    barwidget: add current workspace widget

--- dooit_extras/bar_widgets/__init__.py
+++ dooit_extras/bar_widgets/__init__.py
@@ -3,6 +3,7 @@ from .workspace_progress import WorkspaceProgress
 from .spacer import Spacer
 from .clock import Clock
 from .date import Date
+from .current_workspace import CurrentWorkspace
 
 
-__all__ = ["Mode", "WorkspaceProgress", "Spacer", "Clock", "Date"]
+__all__ = ["Mode", "WorkspaceProgress", "Spacer", "Clock", "Date", "CurrentWorkspace"]
---
+++ dooit_extras/bar_widgets/current_workspace.py
@@ -0,0 +1,50 @@
+from typing import Union
+from dooit.ui.api import DooitAPI
+from dooit.ui.api.events import subscribe
+from dooit.ui.events.events import Startup, WorkspaceSelected
+from rich.text import Text
+from rich.style import Style
+
+from ._base import BarUtilWidgetBase
+
+
+@subscribe(WorkspaceSelected, Startup)
+def get_workspace_name(_: DooitAPI, event: Union[WorkspaceSelected, Startup]) -> str:
+    if isinstance(event, Startup):
+        return ""
+
+    text = event.workspace.description
+    parent = event.workspace.parent_workspace
+
+    if parent and not parent.is_root:
+        text = f"{parent.description} / {text}"
+
+    return text
+
+
+class CurrentWorkspace(BarUtilWidgetBase):
+    def __init__(
+        self,
+        api: DooitAPI,
+        no_workspace_text="No Workspace Selected",
+        fmt=" {} ",
+        fg: str = "",
+        bg: str = "",
+    ) -> None:
+        super().__init__(func=get_workspace_name, api=api, width=None, fmt=fmt)
+
+        self.fg = fg
+        self.bg = bg
+        self.no_workspace_text = no_workspace_text
+
+    @property
+    def value(self) -> str:
+        value = super().value if super().value.strip() else self.no_workspace_text
+        return self.fmt.format(value)
+
+    def render(self) -> Text:
+        fg = self.fg or self.api.app.current_theme.background_1
+        bg = self.bg or self.api.app.current_theme.primary
+        style = Style(color=fg, bgcolor=bg)
+
+        return self.render_text(style=style)
