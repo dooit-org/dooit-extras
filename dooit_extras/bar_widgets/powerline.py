45b0b7c193a7af45656db6e7d738b76e621c5f05
kraanzu <kraanzu@gmail.com>
2024-10-23 20:40:26 +0530

    add powerline separators

--- dooit_extras/bar_widgets/powerline.py
+++ dooit_extras/bar_widgets/powerline.py
@@ -1,5 +1,70 @@
-from ._base import BarUtilWidgetBase
+from dooit.ui.api import DooitAPI
+from dooit.ui.tui import Dooit
+from .text_box import TextBox
+from typing_extensions import Self
 
 
-class Powerline(BarUtilWidgetBase):
-    pass
+class Powerline(TextBox):
+    def __init__(
+        self,
+        api: DooitAPI = DooitAPI(Dooit()),
+        text: str = "",
+        fmt: str = " {} ",
+        fg: str = "",
+        bg: str = "",
+    ) -> None:
+        super().__init__(api, text, fmt, fg, bg)
+
+    # Lower Triangles
+    @classmethod
+    def lower_left_triangle(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, "", fg=fg, bg=bg)
+
+    @classmethod
+    def lower_right_triangle(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, "", fg=fg, bg=bg)
+
+    # Upper Triangles
+    @classmethod
+    def upper_left_triangle(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, "", fg=fg, bg=bg)
+
+    @classmethod
+    def upper_right_triangle(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, "", fg=fg, bg=bg)
+
+    # Arrows
+    @classmethod
+    def right_arrow(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, "", fg=fg, bg=bg)
+
+    @classmethod
+    def left_arrow(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, "", fg=fg, bg=bg)
+
+    # Rounded
+    @classmethod
+    def right_rounded(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, "", fg=fg, bg=bg)
+
+    @classmethod
+    def left_rounded(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, "", fg=fg, bg=bg)
+
+    # Ice
+    @classmethod
+    def left_ice(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, " ", fg=fg, bg=bg)
+
+    @classmethod
+    def right_ice(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, " ", fg=fg, bg=bg)
+
+    # Fire
+    @classmethod
+    def left_flame(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, " ", fg=fg, bg=bg)
+
+    @classmethod
+    def right_flame(cls, api: DooitAPI, fg: str = "", bg: str = "") -> Self:
+        return cls(api, " ", fg=fg, bg=bg)
