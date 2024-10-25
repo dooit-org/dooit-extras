faaa90c04626bb0ec351578fa2ca042e2cb9a668
kraanzu <kraanzu@gmail.com>
2024-10-25 19:00:13 +0530

    formatter: add status icons

--- dooit_extras/formatters/__init__.py
+++ dooit_extras/formatters/__init__.py
@@ -1,9 +1,11 @@
 from .description import description_highlight_link, description_children_count
 from .due import due_danger_today, due_causal_format
+from .status import status_icons
 
 __all__ = [
     "description_highlight_link",
     "description_children_count",
     "due_danger_today",
     "due_causal_format",
+    "status_icons",
 ]
---
+++ dooit_extras/formatters/status.py
@@ -0,0 +1,41 @@
+from typing import Callable, Dict
+from rich.style import Style
+from rich.text import Text
+from dooit.api import Todo
+from dooit.ui.api import DooitAPI
+
+
+def status_icons(
+    icons: Dict[str, str], colors: Dict[str, Style] = {}, fmt="{}"
+) -> Callable:
+    """
+    Shows status icons for todos
+    """
+
+    def wrapper(status: str, _: Todo, api: DooitAPI) -> Text:
+        """
+        Shows icon for various statuses
+        """
+
+        theme = api.vars.theme
+        default_styles = {
+            "completed": Style(color=theme.green, bold=True),
+            "pending": Style(color=theme.yellow, bold=True),
+            "overdue": Style(color=theme.red, bold=True),
+        }
+
+        default_icons = {
+            "completed": "x",
+            "pending": "o",
+            "overdue": "!",
+        }
+
+        default_styles.update(colors)
+        default_icons.update(icons)
+
+        icon = default_icons[status]
+        style = default_styles[status]
+
+        return Text(fmt.format(icon), style=style)
+
+    return wrapper
