cc9fe885a70cfbbe7d7dad82f89308fdfdd873de
kraanzu <kraanzu@gmail.com>
2024-10-25 18:32:15 +0530

    formatters: add children count

--- dooit_extras/formatters/__init__.py
+++ dooit_extras/formatters/__init__.py
@@ -1,8 +1,9 @@
-from .description import description_highlight_link
+from .description import description_highlight_link, description_children_count
 from .due import due_danger_today, due_causal_format
 
 __all__ = [
-    "due_danger_today",
     "description_highlight_link",
+    "description_children_count",
+    "due_danger_today",
     "due_causal_format",
 ]
--- dooit_extras/formatters/description.py
+++ dooit_extras/formatters/description.py
@@ -1,9 +1,13 @@
+from typing import Union
+from dooit.api import Todo, Workspace
 from rich.style import Style
 from dooit.ui.api import DooitAPI
 from rich.style import Style
 from rich.text import Text
 from dooit.ui.api import DooitAPI, allow_multiple_formatting
 import re
+
+ModelType = Union[Todo, Workspace]
 
 
 @allow_multiple_formatting
@@ -30,3 +34,23 @@ def description_highlight_link(value: str, _, api: DooitAPI):
     )
 
     return text.markup
+
+
+def description_children_count(format: str = " ({}) "):
+    @allow_multiple_formatting
+    def wrapper(value: str, model: ModelType):
+        """
+        Highlight the number of children in the description.
+        """
+
+        if isinstance(model, Todo):
+            children_count = len(model.todos)
+        else:
+            children_count = len(model.workspaces)
+
+        if not children_count:
+            return
+
+        return value + format.format(children_count)
+
+    return wrapper
