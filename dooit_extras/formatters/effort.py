764217b362836c77306e70e222ce9aab4759ec67
kraanzu <kraanzu@gmail.com>
2024-11-03 22:26:20 +0530

    fix: effort icon

--- dooit_extras/formatters/effort.py
+++ dooit_extras/formatters/effort.py
@@ -4,15 +4,20 @@ from rich.text import Text
 from dooit.api import Todo
 from dooit.ui.api import DooitAPI, extra_formatter
 
-def effort_icon(icon: str = "󰈸 ", color: Optional[str] = None, show_on_zero: bool = True):
 
+def effort_icon(
+    icon: str = "󰈸 ",
+    color: Optional[str] = None,
+    show_on_zero: bool = True,
+):
     @extra_formatter
     def wrapper(value: str, model: Todo, api: DooitAPI):
         theme = api.vars.theme
 
-        if model.effort is None or (not show_on_zero and model.effort == 0):
-            return value
+        if not show_on_zero and model.effort == 0:
+            return ""
 
         return Text() + Text(icon, style=Style(color=color or theme.orange)) + value
 
     return wrapper
+
