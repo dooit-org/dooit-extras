674159abd4842fd0080094582e89bed40a176789
kraanzu <kraanzu@gmail.com>
2024-11-03 22:21:26 +0530

    optimize: css inject is slow

--- dooit_extras/scripts/toggle_workspaces.py
+++ dooit_extras/scripts/toggle_workspaces.py
@@ -1,28 +1,19 @@
 from dooit.ui.api import DooitAPI
 
-CSS = """
-#workspace_switcher {
-    display: none;
-}
-
-#todo_switcher {
-    column-span: 2;
-}
-"""
-
-CSS_ID = "toggle_workspaces"
 
 def toggle_workspaces(api: DooitAPI):
     def wrapper():
         """Toggles the visibility of the workspaces tree"""
+        workspace_switcher = api.app.query_one("#workspace_switcher")
+        todo_switcher = api.app.query_one("#todo_switcher")
 
-        if api.css.is_active(CSS_ID):
-            api.css.unject_css(CSS_ID)
+        if workspace_switcher.display:
+            workspace_switcher.display = False
+            todo_switcher.styles.column_span = 2
+            api.switch_focus()
+        else:
+            workspace_switcher.display = True
+            todo_switcher.styles.column_span = 1
             api.app.workspace_tree.focus()
 
-        else:
-            if api.app.workspace_tree.has_focus:
-                api.switch_focus()
-
-            api.css.inject_css(CSS, CSS_ID)
     return wrapper
