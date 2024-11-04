d2d58d1e3c94b71c624e9f4deb6de83398807726
kraanzu <kraanzu@gmail.com>
2024-11-04 10:12:26 +0530

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -9,57 +9,59 @@
     self,
     nixpkgs,
     flake-utils,
-    dooit
-  }
-  :
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        name = "dooit-extras";
+    dooit,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      python3 = pkgs.python312Packages;
+    in {
+      packages.default = python3.buildPythonPackage {
+        pname = "dooit-extras";
         version = "0.1.0";
+        src = ./.;
 
-        pkgs = import nixpkgs {inherit system;};
-        python3 = pkgs.python312Packages;
+        format = "pyproject";
 
-        mainPkgs = with python3; [
+        # Dependencies for building the package
+        nativeBuildInputs = with pkgs; [
+          poetry
+          dooit.packages.${system}.default
+        ];
+
+        # Dependencies for runtime and development
+        buildInputs = with python3; [
           textual
           poetry-core
         ];
-      in {
-        packages.default = python3.buildPythonPackage {
-          pname = name;
-          version = version;
-          src = ./.;
-          format = "pyproject";
 
-          nativeBuildInputs = with pkgs; [
-            poetry
-            dooit.packages.${system}.default
-          ];
+        doCheck = true; # Enable tests
+        checkInputs = with python3; [
+          pytest
+          pytest-asyncio
+          coverage
+          faker
+        ];
+      };
 
-          pythonRelaxDeps = [
-            "textual"
-          ];
+      # Development environment
+      devShell = pkgs.mkShell {
+        name = "dooit-extras-dev";
+        buildInputs =
+          (with python3; [
+            ruff
+            pre-commit
+            textual-dev
+            pytest
+            coverage
+            faker
+          ])
+          ++ [pkgs.bun];
 
-          buildInputs = mainPkgs;
-
-          doCheck = false; # no tests
-        };
-
-        # Deps: Devshell
-        devShell = pkgs.mkShell {
-          name = "dooit-extras";
-          buildInputs =
-            mainPkgs
-            ++ (with pkgs; [
-              bun
-            ]);
-
-          shellHook = ''
-            cd site
-            ${pkgs.bun}/bin/bun install
-            cd ..
-          '';
-        };
-      }
-    );
+        shellHook = ''
+          cd site
+          ${pkgs.bun}/bin/bun install
+          cd ..
+        '';
+      };
+    });
 }
