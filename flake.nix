e6c80c530cdc61c5877d2fd95f44612959faa2d1
kraanzu <kraanzu@gmail.com>
2024-11-07 17:36:59 +0530

    flake: im dumb

--- flake.nix
+++ flake.nix
@@ -3,12 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # dooit.url = "github:kraanzu/dooit/develop";
+    dooit.url = "github:kraanzu/dooit/develop";
   };
 
   outputs = {
     self,
     nixpkgs,
+    dooit,
     ...
   }: let
     forEachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
@@ -20,7 +21,7 @@
         }
     );
 
-    packageFor = system: pkgsFor.${system}.callPackage ./nix {};
+    packageFor = system: pkgsFor.${system}.callPackage ./nix {dooit = dooit.packages.${system}.default;};
   in {
     packages = forEachSystem (
       system: {
--- nix/default.nix
+++ nix/default.nix
@@ -2,11 +2,11 @@
   lib,
   fetchFromGitHub,
   python311,
-  # dooit,
+  dooit,
 }: let
   python3 = python311;
 in
-  python3.pkgs.buildPythonApplication rec {
+  python3.pkgs.buildPythonPackage {
     pname = "dooit-extras";
     version = "0.1.0";
     pyproject = true;
@@ -15,26 +15,11 @@ in
       owner = "dooit-org";
       repo = "dooit-extras";
       rev = "main";
-      hash = "sha256-N0r37iEj0P/qLBFG9bGPUPjw31Wk9kA+rrZd59Yrxd4=";
+      hash = "sha256-FsgR/2fEC/6uxbyUm7CC0st+s4ASp0HNGBoTssEVijc=";
     };
 
     build-system = with python3.pkgs; [poetry-core];
-    pythonRelaxDeps = [
-      "tzlocal"
-      "textual"
-    ];
-
-    propagatedBuildInputs = with python3.pkgs; [
-      pyperclip
-      textual
-      pyyaml
-      dateutil
-      sqlalchemy
-      platformdirs
-      tzlocal
-      click
-    ];
-
+    buildInputs = [dooit];
     # No tests available
     doCheck = false;
 
