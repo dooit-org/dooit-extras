faab40ab94b1d2ba0a92cb951a6c5ea3926feb8b
kraanzu <kraanzu@gmail.com>
2025-05-31 10:20:54 +0530

    nix: Bump version

--- nix/default.nix
+++ nix/default.nix
@@ -5,18 +5,18 @@
   dooit,
 }: let
   python3 = python311;
-  version = "0.2.0";
+  version = "0.2.1";
 in
   python3.pkgs.buildPythonPackage {
     pname = "dooit-extras";
-    version = "0.2.0";
+    version = "0.2.1";
     pyproject = true;
 
     src = fetchFromGitHub {
       owner = "dooit-org";
       repo = "dooit-extras";
       rev = "refs/tags/v${version}";
-      hash = "sha256-ZBzzH8k4oC3ovLF9+9rzzFZaDDoOvcbX5NCh1WBedK4=";
+      hash = "sha256-h29lN32Qca8edF1aLhLxnV97MMEapX3Docc+CIEF6I4=";
     };
 
     build-system = with python3.pkgs; [poetry-core];
